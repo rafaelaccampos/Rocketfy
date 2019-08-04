2d55cb7fc4e946582a9b1552629f64ead8e724a6
Rafaela Campos <rafaelacassianocampos1996@gmail.com>
2019-08-04 10:31:30 -0300

    Creating Creating the structure of the main elements of the application

---
+++ src/components/Card/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import List from '../List';
+
+import { Container } from './styles';
+
+export default function Card() {
+    return (
+        <Container>
+
+        </Container>
+    );
+}

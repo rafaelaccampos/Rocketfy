32dd7918700b1dada43e58260ee8836b403892e0
Rafaela Campos <rafaelacassianocampos1996@gmail.com>
2019-08-05 22:12:47 -0300

    Styling the List and Card

--- src/components/Board/styles.js
+++ src/components/Board/styles.js
@@ -5,35 +5,6 @@ export const Container = styled.div`
     padding: 30px 0;
     height: calc(100% - 80px);
 
-    & + div {
-        border-left: 1px solid rgba(0, 0, 0, 0.05);
-    }
-
-    header{
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        height: 42px;
-    }
-
-    h2 {
-        font-weight: 500;
-        font-size: 16px;
-        padding: 0 10px;
-    }
-
-    button {
-        width: 42px;
-        height: 42px;
-        border-radius: 18px;
-        background: #3b5bfd;
-        border: 0;
-        cursor: pointer;
-    }
-
-    ul {
-        margin-top: 30px;
-    }
 `;
 
 // & + div => quero estilizar toda div que antes dela tenha uma div

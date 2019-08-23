75a7bba4079492f134efb2479294c28b15f1c9e8
Rafaela Campos <rafaelacassianocampos1996@gmail.com>
2019-08-23 20:28:26 -0300

    Finishing the part of drag n' drop

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,13 +1,16 @@
-import React, { useRef } from 'react';
+import React, { useRef, useContext } from 'react';
 import { useDrag, useDrop } from 'react-dnd';
+
+import BoardContext from '../Board/context';
 
 import { Container, Label } from './styles';
 
-export default function Card({ data, index }) {
+export default function Card({ data, index, listIndex }) {
     const ref = useRef();
-    
+    const { move } = useContext(BoardContext);
+
     const [ { isDragging }, dragRef] = useDrag({
-            item: { type: 'CARD', index },
+            item: { type: 'CARD', index, listIndex },
             collect: monitor => ({
                 isDragging: monitor.isDragging(),
             }),
@@ -16,10 +19,14 @@ export default function Card({ data, index }) {
     const [, dropRef] = useDrop({
         accept: 'CARD',
         hover(item, monitor) {
+            
+           const draggedListIndex = item.listIndex;
+           const targetListIndex = listIndex;
+
            const draggedIndex = item.index;
            const targetIndex = index;
 
-           if(draggedIndex === targetIndex){
+           if(draggedIndex === targetIndex && draggedListIndex === targetListIndex ){
                return;
            }
 
@@ -29,7 +36,18 @@ export default function Card({ data, index }) {
            const draggedOffset = monitor.getClientOffset();
            const draggedTop = draggedOffset.y - targetSize.top;
 
-           console.log(draggedTop, targetCenter);
+           if(draggedIndex < targetIndex && draggedTop < targetCenter){
+               return;
+           }
+
+           if(draggedIndex > targetIndex && draggedTop > targetCenter){
+               return;
+           }
+           
+           move(draggedListIndex, targetListIndex, draggedIndex, targetIndex);
+
+           item.index = targetIndex;
+           item.listIndex = targetListIndex;
         }
     })
 
